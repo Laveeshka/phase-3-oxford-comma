2568dfa6e564ccf85ecd8ce5c4063381066e2df8
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-03 16:39:39 +1100

    commit tree fce9c9cae9a22ecdef1d6523983eaa53e5e14ce2

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        array.join(" and ")
+    elsif array.size == 3
+        #remove last element
+        lastElem = ", and " + array.pop 
+        result =  array.join(", ")
+        result << lastElem
+        result
+    else 
+        #remove last element
+        lastElem = ", and " + array.pop 
+        result =  array.join(", ")
+        result << lastElem
+        result
+    end
 end
